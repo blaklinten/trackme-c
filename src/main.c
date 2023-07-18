ff9e816d19663d37e3d8ecfb3255da1b6b002efc
Lucas Nilsson <lucas@fripost.org>
2023-07-18 10:24:50 +0200

    Add libgit2 and mongoose support to project

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-#define EXIT_SUCCESS 0;
+#include "libgit2/include/git2.h"
+#include "mongoose.h"
 
 int main(int argc, char *argv[]) {
-  printf("Hello trackMe!\n");
+  git_libgit2_init();      // init libgit2
+  struct mg_mgr mgr;       // Event manager
+  mg_log_set(MG_LL_DEBUG); // Set log level
+  mg_mgr_init(&mgr);       // Initialise event manager
+  printf("Hello Git and Mongoose!\n");
+  git_libgit2_shutdown();
+  mg_mgr_init(&mgr);
   return EXIT_SUCCESS;
 }
