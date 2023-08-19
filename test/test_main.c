e3187a2a3772ad389d9a46f28e36893709024c42
Lucas Nilsson <lucas@fripost.org>
2023-08-19 23:34:04 +0200

    add initial database tests

--- test/test_main.c
+++ test/test_main.c
@@ -7,6 +7,7 @@
 #include <cmocka.h>
 
 #include "test_timer.h"
+#include "test_db.h"
 
 
 int main(void) {
@@ -15,6 +16,11 @@ int main(void) {
       cmocka_unit_test(timer_reset),
       cmocka_unit_test(timer_start),
       cmocka_unit_test(timer_stop),
+      cmocka_unit_test(db_connect),
+      cmocka_unit_test(db_save),
+      cmocka_unit_test(db_get_by_name),
+      cmocka_unit_test(db_get_by_client),
+      cmocka_unit_test(db_get_by_project),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
