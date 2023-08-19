e071b428af93e502de826b21622819f868b549fc
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2023-08-19 23:34:04 +0200

    add initial database tests

---
+++ src/db.h
@@ -0,0 +1,7 @@
+#ifndef DB_H
+#define DB_H
+
+#include <mongoc/mongoc.h>
+
+#endif // !DB_H
+
---
+++ test/test_db.c
@@ -0,0 +1,19 @@
+#include "db.h"
+#include <libmongoc-1.0/mongoc.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <cmocka.h>
+
+void db_connect() { fail(); }
+
+void db_save() { fail(); }
+
+void db_get_by_name() { fail(); }
+
+void db_get_by_client() { fail(); }
+
+void db_get_by_project() { fail(); }
