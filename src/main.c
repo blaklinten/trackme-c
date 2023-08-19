93ab006f93a3b28efa15a173aa3bf3e333b356a9
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2023-08-19 23:03:42 +0200

    improve timer test
        - rename timer test functions
    
        - add timer to main to make sure it is linked

--- src/main.c
+++ src/main.c
@@ -3,12 +3,18 @@
 #include "libgit2/include/git2.h"
 #include "mongoose.h"
 
+#include "timer.h"
+
 int main(int argc, char *argv[]) {
   git_libgit2_init();      // init libgit2
   struct mg_mgr mgr;       // Event manager
   mg_log_set(MG_LL_DEBUG); // Set log level
   mg_mgr_init(&mgr);       // Initialise event manager
   printf("Hello Git and Mongoose!\n");
+
+  Timer t;
+  reset(&t);
+
   git_libgit2_shutdown();
   mg_mgr_free(&mgr);
   return EXIT_SUCCESS;
--- test/test_timer.h
+++ test/test_timer.h
@@ -1,8 +1,8 @@
 #ifndef TEST_TIMER_H
 #define TEST_TIMER_H
 
-void timer_reset_test();
-void timer_start_test();
-void timer_stop_test();
+void timer_reset();
+void timer_start();
+void timer_stop();
 
 #endif // !TEST_TIMER_H
