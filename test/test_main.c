3c97851a52fb257cbc75c8c8a4816b2cd888f80c
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2023-11-07 22:16:07 +0100

    Remove unecessary tests to better see what is going on.

--- test/test_main.c
+++ test/test_main.c
@@ -17,10 +17,6 @@ int main(void) {
       cmocka_unit_test(timer_start),
       cmocka_unit_test(timer_stop),
       cmocka_unit_test(db_connect),
-      cmocka_unit_test(db_save),
-      cmocka_unit_test(db_get_by_name),
-      cmocka_unit_test(db_get_by_client),
-      cmocka_unit_test(db_get_by_project),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
