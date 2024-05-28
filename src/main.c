21c7ec6c73ec38f485773976e30cb89c1f20f46b
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2024-05-28 21:28:41 +0200

    Remove libgit
    Unecessary git dependency, we will use mongoDB instead

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-#include "libgit2/include/git2.h"
 #include "mongoose.h"
 
 #include "timer.h"
 
 int main(int argc, char *argv[]) {
-  git_libgit2_init();      // init libgit2
   struct mg_mgr mgr;       // Event manager
   mg_log_set(MG_LL_DEBUG); // Set log level
   mg_mgr_init(&mgr);       // Initialise event manager
@@ -15,7 +13,6 @@ int main(int argc, char *argv[]) {
   Timer t;
   reset(&t);
 
-  git_libgit2_shutdown();
   mg_mgr_free(&mgr);
   return EXIT_SUCCESS;
 }
