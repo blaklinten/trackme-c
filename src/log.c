bd94d9cd35abd993a353bfe7f9ae38a6a4002e4b
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2024-07-13 11:46:04 +0200

    Add logging functions
    t_log functions for ERROR and INFO log levels

---
+++ src/log.c
@@ -0,0 +1,32 @@
+#include "log.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void t_log(LOG_LEVEL level, const char *func, ...) {
+  char *system_log_level = getenv("TRACKME_LOG_LEVEL");
+  if ((!system_log_level) || atoi(system_log_level) < (int)level) {
+    return;
+  }
+  va_list args;
+  va_start(args, func);
+  char *info = va_arg(args, char *);
+
+  char *format;
+  switch (level) {
+  case INFO:
+    format = ANSI_COLOR_YELLOW "[%s] Info: " ANSI_COLOR_RESET "%s\n";
+    break;
+  case ERROR:
+    format = ANSI_COLOR_RED "[%s] Error: " ANSI_COLOR_RESET "%s\n";
+    break;
+  }
+
+  int length = strlen(format) + strlen(func) + strlen(info);
+  char *log_format = malloc(length);
+  snprintf(log_format, length, format, func, info);
+  vprintf(log_format, args);
+  va_end(args);
+  free(log_format);
+}
