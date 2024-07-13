a922ca94d4f9a454d91f005b7c48dc1864c79bdc
Lenny <lucas@fripost.org>
2024-07-13 11:46:04 +0200

    Add logging functions
    t_log functions for ERROR and INFO log levels

---
+++ src/log.h
@@ -0,0 +1,13 @@
+#ifndef LOG_H
+#define LOG_H
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+typedef enum { ERROR, INFO } LOG_LEVEL;
+
+void t_log(LOG_LEVEL, const char *, ...);
+
+#endif // !LOG_H
