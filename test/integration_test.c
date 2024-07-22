7cea9a896ccfb82260e47aa5b67b2a2f4dcdb4bd
Lenny <lucas@fripost.org>
2024-07-22 22:07:06 +0200

    Add code to compare entries

--- test/integration_test.c
+++ test/integration_test.c
@@ -13,12 +13,23 @@ static int group_setup(void **state) {
   init_db(uri_string);
   mongoc_collection_drop(entries, NULL);
 
+  s->TEST_NAME_1 = "Lucas";
+  s->TEST_NAME_2 = "Jonas";
+  s->TEST_CLIENT = "test client";
+  s->TEST_PROJECT = "test project";
+  s->TEST_START_TIME_S = 1721664594; //Mon 22 Jul 18:09:54 CEST 2024
+  s->TEST_END_TIME_S = 1721669586; //Mon 22 Jul 19:33:06 CEST 2024
+  s->TEST_DURATION_S = 60*60 + 60*23 + 12; // 1h23min12s
+
   s->test_document = bson_new();
   bson_oid_init(&s->test_id, NULL);
   BSON_APPEND_OID(s->test_document, DB_KEY_ID, &s->test_id);
-  s->TEST_NAME_1 = "Lucas";
-  s->TEST_NAME_2 = "Jonas";
   BSON_APPEND_UTF8(s->test_document, DB_KEY_NAME, s->TEST_NAME_1);
+  BSON_APPEND_UTF8(s->test_document, DB_KEY_CLIENT, s->TEST_CLIENT);
+  BSON_APPEND_UTF8(s->test_document, DB_KEY_PROJECT, s->TEST_PROJECT);
+  BSON_APPEND_TIME_T(s->test_document, DB_KEY_START_TIME, s->TEST_START_TIME_S);
+  BSON_APPEND_TIME_T(s->test_document, DB_KEY_END_TIME, s->TEST_END_TIME_S);
+  BSON_APPEND_TIME_T(s->test_document, DB_KEY_DURATION, s->TEST_DURATION_S);
 
   *state = s;
 
