2c878e6a9e14e82a41f2cfcea6c41ec1a7c560a5
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2024-08-09 11:24:12 +0200

    Add stubs for linked list test and clean up
    - bson_t_list test stubs
    - re-organize CMakeList.txt to only include needed files

--- test/test_db.h
+++ test/test_db.h
@@ -1,12 +1,12 @@
 #ifndef TEST_DB_H
 #define TEST_DB_H
 
-#include <bson/bson.h>
-#include <mongoc/mongoc.h>
-
 void test_db_connect();
 void test_db_save(void **);
 void test_db_get_by(void **);
 void test_db_insert_and_get(void **);
+void test_db_create_list(void **);
+void test_db_free_list(void **);
+void test_db_count_list_element(void **);
 
 #endif // !TEST_DB_H
