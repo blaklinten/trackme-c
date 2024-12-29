bcc7ad200c85881ae0be27a15ccc7aa6c9ab228f
Lenny <30445004+blaklinten@users.noreply.github.com>
2024-12-29 14:31:31 +0100

    feat: add create_edit page
    - To make it possible to edit running timer
    - misc changes to index

--- src/web.h
+++ src/web.h
@@ -4,8 +4,11 @@
 void create_index_html(char *name, char *client, char *project,
                        char *description, char *duration);
 void create_start_timer_html();
+void create_edit_html(char *name, char *client, char *project,
+                            char *description, char *start_time, char *end_time);
 void create_stop_timer_html(char *name, char *client, char *project,
                             char *description, char *start_time, char *end_time,
                             char *final_duration);
+void clean_up();
 
 #endif // !WEB_H
